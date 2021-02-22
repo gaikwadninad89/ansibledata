4428b08205680827d926668e53669ed6b1df1deb
NinadG <ninad.gaikwad.89@gmail.com>
2021-02-22 15:07:38 +0530

    Add files via upload
    php files for application

---
+++ welcome.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+
+<?php $name = $_POST["name"]; ?><br>
+<?php $email = $_POST["email"]; ?>
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO table1 (name, email) VALUES ('$name', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
+</body>
+</html> 
